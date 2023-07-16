eae3aa7a4f73a3c86b1655dc0ef6126ea23dac8a
yasminat24-meet <139688683+yasminat24-meet@users.noreply.github.com>
2023-07-16 19:01:54 +0300

    Create pythonreview.py

---
+++ pythonreview.py
@@ -0,0 +1,11 @@
+def create_youtube_video (title, description):
+	youtube_video_dictionary= {"title":title ,"description": description, "likes": 0,"dislikes": 0, "comments":{ "username":username}}
+	return  youtube_video_dictionary
+def like(youtube video):
+	if likes in youtube_video_dictionary:
+		likes+=1
+	return youtube_video_dictionary
+def dislike(youtube video):
+	if dislikes in youtube_video_dictionary:
+		dislikes+=1
+def 
