a76a578e708f076935dd43d442415380e27433da
yasminat24-meet <139688683+yasminat24-meet@users.noreply.github.com>
2023-07-17 12:12:29 +0300

    Update pythonreview.py

--- pythonreview.py
+++ pythonreview.py
@@ -1,11 +1,24 @@
 def create_youtube_video (title, description):
-	youtube_video_dictionary= {"title":title ,"description": description, "likes": 0,"dislikes": 0, "comments":{ "username":username}}
-	return  youtube_video_dictionary
-def like(youtube video):
-	if likes in youtube_video_dictionary:
-		likes+=1
+	youtubevideo= {"title":title ,"description": description, "likes": 0,"dislikes": 0, "comments":{ "username":username}}
+	return  youtubevideo
+def like(video):
+	if likes in video:
+		video["likes"]+=1
 	return youtube_video_dictionary
-def dislike(youtube video):
-	if dislikes in youtube_video_dictionary:
-		dislikes+=1
-def 
+def dislike(video):
+	if dislikes in video:
+		video["dislikes"]+=1
+def add_comment(youtubevideo, username, comment_text):
+	video["comment"]["username"]=comment_text
+	return video
+yv=  Create_youtube_video("cake","sushi")
+print(yv)
+likes(yv)
+dislikes(yv)
+add_comment(yv,"jazz","sushi is amazing")
+print(yv)
+for i in (495):
+	like(yv)
+print(yv)
+
+
